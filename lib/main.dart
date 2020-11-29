d8b1e74f4aea26f94dd394bf2c4aa4280ad058bf
Matheus Gabriel <matheusg663@gmail.com>
2020-11-28 22:29:33 -0300

    28/11/2020

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
+import 'dart:async';
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-import 'dart:async';
-import 'dart:io';
-import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
@@ -17,7 +18,22 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   List _toDoList = [];
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   final _toDoController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _readData().then((data) {
+      setState(() {
+        _toDoList = json.decode(data);
+      });
+    });
+  }
+
   void _addToDo() {
     setState(() {
       Map<String, dynamic> newToDo = Map();
@@ -25,7 +41,26 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
     });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return -1;
+        else
+          return 0;
+      });
+
+      _saveData();
+    });
+
+    return Null;
   }
 
   @override
@@ -60,24 +95,12 @@ class _HomeState extends State<Home> {
             ),
           ),
           Expanded(
-              child: ListView.builder(
-            padding: EdgeInsets.only(top: 10),
-            itemCount: _toDoList.length,
-            itemBuilder: (context, index) {
-              return CheckboxListTile(
-                  onChanged: (c) {
-                    setState(() {
-                      _toDoList[index]["ok"] = c;
-                    });
-                    ;
-                  },
-                  title: Text(_toDoList[index]["title"]),
-                  value: _toDoList[index]["ok"],
-                  secondary: CircleAvatar(
-                    child: Icon(
-                        _toDoList[index]["ok"] ? Icons.check : Icons.error),
-                  ));
-            },
+              child: RefreshIndicator(
+            child: ListView.builder(
+                padding: EdgeInsets.only(top: 10),
+                itemCount: _toDoList.length,
+                itemBuilder: _buildItem),
+            onRefresh: _refresh,
           ))
         ]));
   }
@@ -91,6 +114,58 @@ class _HomeState extends State<Home> {
     String data = json.encode(_toDoList);
     final file = await _getFile();
     return file.writeAsString(data);
+  }
+
+  Widget _buildItem(BuildContext context, int index) {
+    return Dismissible(
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Tarefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+                label: "Desfazer",
+                onPressed: () {
+                  setState(() {
+                    _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                    _saveData();
+                  });
+                }),
+            duration: Duration(seconds: 3),
+          );
+          Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+          color: Colors.redAccent,
+          child: Align(
+            alignment: Alignment(-0.9, 0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            ),
+          )),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+          onChanged: (c) {
+            setState(() {
+              _toDoList[index]["ok"] = c;
+              _saveData();
+            });
+          },
+          title: Text(_toDoList[index]["title"]),
+          value: _toDoList[index]["ok"],
+          secondary: CircleAvatar(
+            child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
+          )),
+    );
   }
 
   Future<String> _readData() async {
